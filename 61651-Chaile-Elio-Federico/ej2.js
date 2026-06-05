3db2ac407531164b4eec21c4a5b58af96743ca40
Federico_Chaile <fchaile35@hotmail.com>
2026-06-04 21:35:54 -0300

    Update ej2.js

--- 61651-Chaile-Elio-Federico/ej2.js
+++ 61651-Chaile-Elio-Federico/ej2.js
@@ -3,17 +3,17 @@ const btnRojo = document.getElementById("btnRojo");
 const btnAmarillo = document.getElementById("btnAmarillo");
 const btnVerde = document.getElementById("btnVerde");
 
-// Evento para el botón Rojo
+
 btnRojo.addEventListener("click", function() {
     cuadrado.style.backgroundColor = "red"; // 
 });
 
-// Evento para el botón Amarillo
+
 btnAmarillo.addEventListener("click", function() {
     cuadrado.style.backgroundColor = "yellow"; // 
 });
 
-// Evento para el botón Verde
+
 btnVerde.addEventListener("click", function() {
     cuadrado.style.backgroundColor = "green"; // 
 });
