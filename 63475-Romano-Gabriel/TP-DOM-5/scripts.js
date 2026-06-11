f1d0ead4a3fa10364aa89a4860b4ee3baa5141d0
romanogabrielnicolas-hue <romanogabrielnicolas@gmail.com>
2026-06-11 16:39:23 -0300

    Trabajo Practico resulto

---
+++ 63475-Romano-Gabriel/TP-DOM-5/scripts.js
@@ -0,0 +1,31 @@
+const inputNombre = document.getElementById('inputNombre');
+const btnAgregar = document.getElementById('btnAgregar');
+const listaInvitados = document.getElementById('listaInvitados');
+const aviso = document.getElementById('aviso');
+
+btnAgregar.addEventListener('click', () => {
+    const nombre = inputNombre.value.trim();
+
+    if (nombre === '') {
+        aviso.textContent = 'Por favor, ingrese un nombre válido.';
+        return;
+    }
+    aviso.textContent = '';
+
+const li = document.createElement('li');
+li.textContent = nombre;
+
+const btnQuitar = document.createElement('button');
+btnQuitar.textContent = 'Quitar';
+
+btnQuitar.addEventListener('click', () => {
+    listaInvitados.removeChild(li);
+});
+
+li.appendChild(btnQuitar);
+listaInvitados.appendChild(li);
+
+inputNombre.value = '';
+inputNombre.focus();
+});
+
