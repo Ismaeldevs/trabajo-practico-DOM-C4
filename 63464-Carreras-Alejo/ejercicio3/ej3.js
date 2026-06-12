499226e07c0bd7e8eab6cde68094e55665e4af48
alejocarreras03 <alejocarreras021@gmail.com>
2026-06-11 23:12:39 -0300

    trabajo practico resuelto

---
+++ 63464-Carreras-Alejo/ejercicio3/ej3.js
@@ -0,0 +1,27 @@
+const inputUsuario = document.getElementById("usuario");
+const inputContrasenia = document.getElementById("contrasenia");
+const btnLogin = document.getElementById("btnLogin");
+const mensaje = document.getElementById("mensaje");
+
+inputUsuario.addEventListener("input", function() {
+mensaje.textContent = "";
+});
+inputContrasenia.addEventListener("input", function() {
+mensaje.textContent = "";
+});
+btnLogin.addEventListener("click", function() {
+const usuario = inputUsuario.value.trim();
+const contrasenia = inputContrasenia.value.trim();
+if  (usuario === "" || contrasenia === ""){
+    mensaje.textContent= "Completa todos los campos"
+    mensaje.style.color= "red"
+} 
+else if (usuario === "admin" || contrasenia === "1234"){
+    mensaje.textContent= "Bienvenido admin"
+    mensaje.style.color= "green"
+}
+else {
+    mensaje.textContent= "Credenciales Incorrectas"
+    mensaje.style.color= "red"
+}
+});
